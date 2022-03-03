c6f7087930b23e1a29e5f5d44965bb701f877cd3
David Mahbubi <ulrichdavid0370@gmail.com>
2022-03-03 22:31:05 +0700

    Fixed assets

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+const typedOptions = {
+  strings: ['Nice to Meet You ! ', 'Have a Great Day !'],
+  typeSpeed: 50,
+  backSpeed: 10,
+  loop: false,
+  startDelay: 100,
+};
+
+const typed = new Typed('.typed-here', typedOptions);
